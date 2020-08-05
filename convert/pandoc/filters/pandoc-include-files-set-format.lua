3e6c1997c896d4dc4f7c5e5615a83a5610b5453e
Gabriel Nützi <gnuetzi@gmail.com>
2020-08-06 01:25:48 +0200

    First implementation
    - pandoc works
    - math works
    - latex macros not included yet
    - set default format filter works

--- convert/pandoc/filters/pandoc-include-files-set-format.lua
+++ convert/pandoc/filters/pandoc-include-files-set-format.lua
@@ -1,7 +1,27 @@
 -- https://github.com/pandoc/lua-filters/issues/101
-function CodeBlock (cb)
-  if cb.classes:includes('.include') then
-    cb.attributes.format = 'markdown+emoji'
+
+format = nil
+
+function setFormatIncludeBlock (cb)
+  if cb.classes:includes('include') and cb.attributes['format'] == nil then
+    cb.attributes.format = format
   end
   return cb
 end
+
+
+function setFormat(doc)
+    format = doc.meta["include-format"]
+    print("Settings include blocks to format" .. format)
+
+    local blks =  pandoc.walk_block(
+      pandoc.Div(doc.blocks),
+      { CodeBlock = setFormatIncludeBlock }
+    ).content
+
+    return pandoc.Pandoc(blks, doc.meta)
+end
+
+return {
+  {Pandoc = setFormat}
+}
